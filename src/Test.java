b01c2547ce4f0fd81b370e423fbee7430ddcba4e
pintuprime <140555986+pintuprime@users.noreply.github.com>
2024-01-27 15:09:41 +0000

    commit 2

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("My name is Hiren");
+        System.out.println("My name is Hiren,how are you");
+        
     }
 }
