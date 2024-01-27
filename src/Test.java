99b0f5762cb575f8cf5752a17b8cc786d67fae5e
pintuprime <pintu.prime@25gmail.com>
2024-01-27 15:13:55 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,8 @@
 public class Test {
 
     public static void main(String[] args) {
+        System.out.println("My name is Hiren, Hello");
         System.out.println("My name is Hiren,how are you");
-        
+
     }
 }
